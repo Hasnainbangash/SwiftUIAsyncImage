151f390a61d08fec5797e3166f1c54554fd960e3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-07 17:29:28 +0500

    Initial Commit

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Elexoft on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
