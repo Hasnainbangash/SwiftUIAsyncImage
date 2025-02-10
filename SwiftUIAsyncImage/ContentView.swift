716a004b550b8968bf77ff1bc243ea34cab471e4
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-10 10:18:05 +0500

    Added the codefor the nimation

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -41,15 +41,52 @@ struct ContentView: View {
         
         // MARK: - 3. PLACEHOLDER
         
+        /*
         AsyncImage(url: URL(string: imageURL)) { image in
             image.imageModifier()
         } placeholder: {
             Image(systemName: "photo.circle.fill").iconModifier()
         }
         .padding(40)
+        */
         
+        // MARK: - 4. PHASE
         
-
+        /*
+        AsyncImage(url: URL(string: imageURL)) { phase in
+            // SUCCESS: The image successfully loaded.
+            // FAILURE: The image failed to load with an error.
+            // Empty: No image is loaded.
+            
+            if let image = phase.image {
+                image.imageModifier()
+            } else if phase.error != nil {
+                Image(systemName: "ant.circle.fill").iconModifier()
+            } else {
+                Image(systemName: "photo.circle.fill").iconModifier()
+            }
+            
+        }.padding(40)
+        */
+        
+        // MARK: - 5. ANIMATION
+        
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+                    .transition(.move(edge: .bottom))
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding()
+        
     }
 }
 
